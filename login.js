1d1c01f18a871a761e373b6069aba0163349155c
youssef-nachar <156086960+youssef-nachar@users.noreply.github.com>
2025-01-07 11:24:33 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,56 @@
+// script.js
+// document.getElementById('loginForm').addEventListener('submit', function(e) {
+//     e.preventDefault();
+  
+//     const username = document.getElementById('username').value;
+//     const password = document.getElementById('password').value;
+  
+//     if (username === '' || password === '') {
+//       alert('Please fill in both fields!');
+//     } else {
+//       alert('Login successful!');
+//       // You can redirect to another page or perform other actions
+//       window.location.href = 'http://127.0.0.1:5500/index.html'; // Example redirect
+//     }
+//   });
+
+
+  document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    if (username === 'youssefnachar331@gmail.com' || password === 'youssefhala1') {
+        alert('Login successful!');
+      // Redirect to your website after a successful login
+      window.location.href = 'https://youssef-nachar.github.io/vistak/';
+    }
+     else {
+        alert('Please fill in both fields!');
+    }
+    if (username === 'abdelrazak@gmail.com' || password === 'youssefhala1') {
+        alert('Login successful!');
+      // Redirect to your website after a successful login
+      window.location.href = 'https://youssef-nachar.github.io/vistak/';
+    }
+     else {
+        alert('Please fill in both fields!');
+    }
+    if (username === 'abdelrazak@gmail.com' || password === 'youssefhala1') {
+        alert('Login successful!');
+      // Redirect to your website after a successful login
+      window.location.href = 'https://youssef-nachar.github.io/vistak/';
+    }
+     else {
+        alert('Please fill in both fields!');
+    }
+    if (username === 'abdelrazak@gmail.com' || password === 'youssefhala1') {
+        alert('Login successful!');
+      // Redirect to your website after a successful login
+      window.location.href = 'https://youssef-nachar.github.io/vistak/';
+    }
+     else {
+        alert('Please fill in both fields!');
+    }
+  });
